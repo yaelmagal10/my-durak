e44a7f8e2d77f7095f7b7ba991142d2c93fffa47
hugsehbd <yoadyw@gmail.com>
2025-06-11 11:50:11 +0300

    changed variables to private in abstract bot

--- backend/abstract_bot.py
+++ backend/abstract_bot.py
@@ -53,46 +53,49 @@ class AbstractBot(ABC):
     def get_original_index(self, current_index: int) -> int:
         """Get the original index of a player based on its current index.
         If something is wrong, return -1."""
-        if not self.active_players:
+        if not self.__active_players:
             return -1
-        if current_index < 0 or current_index >= len(self.active_players):
+        if current_index < 0 or current_index >= len(self.__active_players):
             return -1
-        return self.active_players[current_index]
+        return self.__active_players[current_index]
     def get_current_index(self, original_index: int) -> int:
         """Get the current index of a player based on its original index.
         If something is wrong, return -1."""
-        if not self.active_players:
+        if not self.__active_players:
             return -1
-        if original_index not in self.active_players:
+        if original_index not in self.__active_players:
             return -1
-        return self.active_players.index(original_index)
+        return self.__active_players.index(original_index)
     def get_hand(self) -> List[Tuple[int, int]]:
         """Get the current hand of the bot."""
-        return self.hand
+        return self.__hand
     def get_kozar_suit(self) -> int:
         """Get the suit of the kozar card."""
-        return self.kozar_card[1] if self.kozar_card else -1
+        return self.__kozar_card[1] if self.__kozar_card else -1
     def get_kozar_card(self) -> Tuple[int, int]:
         """Get the kozar card."""
-        return self.kozar_card if self.kozar_card else (-1, -1)
+        return self.__kozar_card if self.__kozar_card else (-1, -1)
     def get_attacking_table_cards(self) -> List[Tuple[int, int]]:
         """Get the current attacking cards on the table."""
-        return self.table_cards[0]
+        return self.__table_cards[0]
     def get_defending_table_cards(self) -> List[Tuple[int, int]]:
         """Get the current defending cards on the table."""
-        return self.table_cards[1]
+        return self.__table_cards[1]
     def get_my_index(self) -> int:
         """Get the index of the bot in the game."""
-        return self.my_index if hasattr(self, 'my_index') else -1
+        return self.__my_index if hasattr(self, '__my_index') else -1
+    def get_raw_events(self) -> List[Tuple]:
+        """Get all the raw events that the bot has received, by order."""
+        return self.__events if hasattr(self, '__events') else []
 
     def __call__(self, event: Tuple, hand: List, table_cards: List, state: Dict[str, Any]):
         self.__dict__.update(state if state is not None else {})
-        if self.events is None:
-            self.events = []
-        self.events.append(event)
+        if self.__events is None:
+            self.__events = []
+        self.__events.append(event)
         action = event[0] if event else None
-        self.hand = hand
-        self.table_cards = table_cards
+        self.__hand = hand
+        self.__table_cards = table_cards
         ret_dict = {}
         match action:
             case Input_actions.OPTIONAL_ATTACK:
@@ -129,13 +132,13 @@ class AbstractBot(ABC):
             case Input_actions.TO_HAND:
                 self.listen_cards_drawn_to_hand(event[1])
             case Input_actions.GAME_INIT:
-                self.active_players = [i for i in range(event[1])]
-                self.my_index = event[2]
-                self.hand = event[3]
-                self.kozar_card = event[4]
+                self.__active_players = [i for i in range(event[1])]
+                self.__my_index = event[2]
+                self.__hand = event[3]
+                self.__kozar_card = event[4]
                 self.game_init(event[1], event[2], event[3], event[4])
             case Input_actions.WINNER_PASSIVE:
-                self.active_players.pop(event[1])
+                self.__active_players.pop(event[1])
                 self.listen_winner(event[1])
             case _:
                 raise ValueError(f"Unknown action: {action}")
