c9b0063101c4adfb150e67a0362194c8c54f9ffb
Amitai-Levy <aaamiiitaaaiii@gmail.com>
2025-06-24 20:37:31 +0300

    Minor changes

--- backend/durak_game.py
+++ backend/durak_game.py
@@ -13,7 +13,7 @@ MAX_ATTACK_SIZE_AFTER_BURN: int = 6
 MAX_TIME_PER_TURN: float = 0.01
 RANKS: List[str] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS: List[str] = ["♣", "♦", "♥", "♠"]
-USE_TIMING: bool = True
+USE_TIMING: bool = False
 
 
 def pretty_print_state(state):
